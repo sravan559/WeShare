11c66fa3db7667105f5e1576350b83b1039c75c5
Sravan Kumar <sravshan@indiana.edu>
2014-10-31 02:21:24 -0400

    Added jquery files to implement validation and constants class to avoid hard coded string values

---
+++ SourceCode/WeShare.DataAccess/DbConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeShare.DataAccess
+{
+    public static class DbConstants
+    {
+        public static string UspUsers = "usp_users";
+        public static string UspTasks = "usp_tasks";
+        public static string UspTaskAssignment = "usp_task_assignment";
+    }
+}

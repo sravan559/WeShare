b812ca3984805aa33fc5ccca51942954380c3389
Sravan Kumar <sravshan@indiana.edu>
2014-10-31 01:40:11 -0400

    Modified parameter name from EmailId to UserId for the UserInfo object

--- SourceCode/WeShare.BusinessModel/UserInfo.cs
+++ SourceCode/WeShare.BusinessModel/UserInfo.cs
@@ -7,11 +7,34 @@ namespace WeShare.BusinessModel
 {
     public class UserInfo
     {
-        public string EmailId { get; set; }
+        /// <summary>
+        /// Property for User ID (Email Address) of the User
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Property to store and retrieve the full name (FirstName,LastName) of the User
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Property for First Name
+        /// </summary>
         public string FirstName { get; set; }
+        
+        /// <summary>
+        /// Property for Last Name of the user
+        /// </summary>
         public string LastName { get; set; }
+        
+        /// <summary>
+        /// Property for Contact Number
+        /// </summary>
         public string ContactNumber { get; set; }
+        
+        /// <summary>
+        /// Property for Password
+        /// </summary>
         public string Password { get; set; }
     }
 }

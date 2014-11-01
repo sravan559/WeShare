bcf4733b7e5210815957698a0685884ae448e0ac
Varsha Vadaga <vvadaga@indiana.edu>
2014-10-31 21:19:03 -0400

    updated column name
    User_ID updated across the stored procedure usp_task_assignment

--- Database Scripts/usp_task_assignment.sql
+++ Database Scripts/usp_task_assignment.sql
@@ -19,7 +19,7 @@ GO
 CREATE PROCEDURE [dbo].[usp_task_assignment]
 (
 @Task_Id INT=NULL,
-@Email_Id nvarchar(50)=NULL,
+@User_Id nvarchar(50)=NULL,
 @Due_Date DATETIME =NULL,
 @Status NVARCHAR(50)=NULL,
 @Action NVARCHAR(20)
@@ -28,13 +28,13 @@ AS
 BEGIN
 	IF @Action='C'
 		IF EXISTS(SELECT Task_Id from AssignedTasks where Task_Id=@Task_Id)
-			UPDATE AssignedTasks SET Email_Id=@Email_Id,
+			UPDATE AssignedTasks SET User_Id=@User_Id,
 								 Due_Date=@Due_Date,
 								 Status=@Status
 								 WHERE Task_Id=@Task_Id
 		ELSE
-			INSERT INTO AssignedTasks(Task_Id,Email_Id,Due_Date,Status)
-					VALUES(@Task_Id,@Email_Id,@Due_Date,@Status)	
+			INSERT INTO AssignedTasks(Task_Id,User_Id,Due_Date,Status)
+					VALUES(@Task_Id,@User_Id,@Due_Date,@Status)	
 	ELSE IF @Action='UPDATESTATUS'
 		 UPDATE AssignedTasks SET Status=@Status WHERE Task_Id=@Task_Id	
 		 
@@ -42,19 +42,18 @@ BEGIN
 		SELECT Task_Id,Task_Title,Task_Description,Points from Tasks where Task_Id NOT IN (SELECT Task_Id FROM AssignedTasks)
 		
 	ELSE IF @Action='GETASSIGNEDTASKS'
-		SELECT t.Task_Id,Task_Title,u.Email_Id,First_Name+', '+Last_Name as 'User_Name', Due_Date,Status from Tasks t inner join AssignedTasks at on t.Task_Id=at.Task_Id
-		inner join Users u on at.Email_Id=u.Email_Id
+		SELECT t.Task_Id,Task_Title,u.User_Id,First_Name+', '+Last_Name as 'User_Name', Due_Date,Status from Tasks t inner join AssignedTasks at on t.Task_Id=at.Task_Id
+		inner join Users u on at.User_Id=u.User_Id
 				
 	ELSE IF @Action='GetTasksByEmailId'
-		Select t.Task_Id,Task_Title,Task_Description, Due_Date,Status from Tasks t INNER JOIN AssignedTasks at ON t.Task_Id=at.Task_Id 
-		where at.Email_Id=@Email_Id
+		SELECT t.Task_Id,Task_Title,Task_Description, Due_Date,Status 
+		from Tasks t INNER JOIN AssignedTasks at ON t.Task_Id=at.Task_Id 
+		WHERE at.User_Id=@User_Id
 				 	 	
-	ELSE IF @Action='StatusUpdate'
-		Update Tasks set Status='Completed' where Task_id=@Task_id;
 	
-	else if @Action ='GetCompTasksByEmailId'
-	Select Select t.Task_Id,Task_Title,Task_Description, Due_Date,Status from Tasks t INNER JOIN AssignedTasks at ON t.Task_Id=at.Task_Id 
-		where at.Email_Id=@Email_Id and Status='Completed';
+	ELSE IF @Action ='GetCompTasksByEmailId'
+	SELECT t.Task_Id,Task_Title,Task_Description, Due_Date,Status from Tasks t INNER JOIN AssignedTasks at ON t.Task_Id=at.Task_Id 
+		WHERE at.User_Id=@User_Id and Status='Completed';
 END
 
 GO

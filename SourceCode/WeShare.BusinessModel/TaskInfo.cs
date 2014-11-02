5dcb4b66defc676cb0d5daac800d96b936d206d1
Varsha Vadaga <vvadaga@indiana.edu>
2014-11-02 15:16:01 -0500

    added comments

--- SourceCode/WeShare.BusinessModel/TaskInfo.cs
+++ SourceCode/WeShare.BusinessModel/TaskInfo.cs
@@ -7,9 +7,24 @@ namespace WeShare.BusinessModel
 {
     public class TaskInfo
     {
+        /// <summary>
+        /// Property for Task ID of the Task
+        /// </summary>
         public int TaskId { get; set; }
+
+        /// <summary>
+        /// Property for Title of the Task
+        /// </summary>
         public string TaskTitle { get; set; }
+
+        /// <summary>
+        /// Property for Description of the Task
+        /// </summary>
         public string TaskDescription { get; set; }
+
+        /// <summary>
+        /// Property for Points allocated to that task
+        /// </summary>
         public int PointsAllocated { get; set; }
         public string TaskType { get; set; }
         public string TaskRecursive { get; set; }

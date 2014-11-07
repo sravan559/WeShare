85de6cdeeb4fbd9b3c338820daa4c489b27a6650
sandeepgarimella <sandgari@indiana.edu>
2014-11-06 21:27:44 -0500

    Implemented exception handling for Home.aspx.cs

--- SourceCode/WeShare/WebForms/Home.aspx.cs
+++ SourceCode/WeShare/WebForms/Home.aspx.cs
@@ -14,62 +14,98 @@ namespace WeShare.WebForms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            try
             {
-                LoadMyTasks();
-                LoadAllTasks();
+                if (!IsPostBack)
+                {
+                    LoadMyTasks();
+                    LoadAllTasks();
+                }
+            }
+            catch (Exception ex)
+            {
+                ManageException(ex);
             }
         }
 
         private void LoadMyTasks()
         {
-            BLTaskAssignment objBlTasks = new BLTaskAssignment();
-            if (Session["UserId"] == null)
+            try
             {
-                Response.Redirect("Login.aspx?IsSessionExpired=1");
+                BLTaskAssignment objBlTasks = new BLTaskAssignment();
+                if (Session["UserId"] == null)
+                {
+                    Response.Redirect("Login.aspx?IsSessionExpired=1");
+                }
+                List<TaskAssignmentInfo> listTaskInfo = objBlTasks.GetUserTasksByMailId(UserId);
+                gvMyTasks.DataSource = listTaskInfo;
+                gvMyTasks.DataBind();
             }
-            List<TaskAssignmentInfo> listTaskInfo = objBlTasks.GetUserTasksByMailId(UserId);
-            gvMyTasks.DataSource = listTaskInfo;
-            gvMyTasks.DataBind();
+            catch (Exception ex)
+            {
+                ManageException(ex);
+            }
         }
 
 
         private void LoadAllTasks()
         {
-            BLTaskAssignment objBlTasks = new BLTaskAssignment();
-            if (Session["UserId"] == null)
+            try
             {
-                Response.Redirect("Login.aspx");
+                BLTaskAssignment objBlTasks = new BLTaskAssignment();
+                if (Session["UserId"] == null)
+                {
+                    Response.Redirect("Login.aspx");
+                }
+                List<TaskAssignmentInfo> listTaskInfo = objBlTasks.GetAllAssignedTasks();
+                gvAllTasks.DataSource = listTaskInfo;
+                gvAllTasks.DataBind();
             }
-            List<TaskAssignmentInfo> listTaskInfo = objBlTasks.GetAllAssignedTasks();
-            gvAllTasks.DataSource = listTaskInfo;
-            gvAllTasks.DataBind();
+            catch (Exception ex)
+            {
+                ManageException(ex);
+            }
         }
 
         protected void gvMyTasks_RowCommand(object sender, GridViewCommandEventArgs e)
         {
-            if (e.CommandName == "TaskComplete")
+            try
             {
-                GridViewRow currentGridRow = (GridViewRow)((Control)e.CommandSource).NamingContainer;
-                int rowIndex = currentGridRow.RowIndex;
-                int taskId = Convert.ToInt32(gvMyTasks.DataKeys[rowIndex].Values["TaskId"]);
-                // Mark the respective task as completed
-                BLTaskAssignment objBlTasks = new BLTaskAssignment();
-                objBlTasks.UpdateTaskStatus(taskId, "Completed");
-                LoadMyTasks();
+                if (e.CommandName == "TaskComplete")
+                {
+                    GridViewRow currentGridRow = (GridViewRow)((Control)e.CommandSource).NamingContainer;
+                    int rowIndex = currentGridRow.RowIndex;
+                    int taskId = Convert.ToInt32(gvMyTasks.DataKeys[rowIndex].Values["TaskId"]);
+                    // Mark the respective task as completed
+                    BLTaskAssignment objBlTasks = new BLTaskAssignment();
+                    objBlTasks.UpdateTaskStatus(taskId, "Completed");
+                    LoadMyTasks();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ManageException(ex);
             }
         }
 
         protected void gvMyTasks_RowDataBound(object sender, GridViewRowEventArgs e)
         {
-            if (e.Row.RowType == DataControlRowType.DataRow)
+            try
             {
-                string status = gvMyTasks.DataKeys[e.Row.RowIndex].Values["Status"].ToStr();
-                if (status.ToLower() == "complete")
+                if (e.Row.RowType == DataControlRowType.DataRow)
                 {
-                    ImageButton imgMarkComplete = (ImageButton)e.Row.FindControl("imgMarkComplete");
-                    imgMarkComplete.Visible = false;
+                    string status = gvMyTasks.DataKeys[e.Row.RowIndex].Values["Status"].ToStr();
+                    if (status.ToLower() == "complete")
+                    {
+                        ImageButton imgMarkComplete = (ImageButton)e.Row.FindControl("imgMarkComplete");
+                        imgMarkComplete.Visible = false;
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                ManageException(ex);
             }
         }
 

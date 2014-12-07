2dfc04d1e527d62a212c5c94d8b375678fbab518
Sravan Kumar <sravshan@indiana.edu>
2014-12-07 16:13:46 -0500

    Added query to add weekly points recursivly to the due points of the user

--- Database Scripts/usp_users.sql
+++ Database Scripts/usp_users.sql
@@ -49,17 +49,33 @@ BEGIN
 					WHERE User_Id=@User_Id	
 										
 	ELSE IF @Action = 'VALIDATEUSER' -- VERIFIES THE PASSWORD ENTERED BY THE USER
-		BEGIN
+		BEGIN --@Action = 'VALIDATEUSER'
 			IF EXISTS (SELECT USER_ID from Users where User_Id=@User_Id AND password=@Password)
-				SELECT 'TRUE' as 'RESULT'					 
-		END
-	ELSE IF @Action='UPDATEDUEPOINTS' -- USED TO UPDATE THE POITNS DUE BASED ON THE WEEKLY POINTS AND RECURSIVE DATE
-		BEGIN
-			UPDATE UsersInGroups SET Next_Recurrence_Date=@Next_Recurrence_Date,
-								 Points_Due=Points_Due+ @Points_To_Add 
-								 WHERE User_Id=@User_Id and Group_Name=@Group_Name
-		END
-				
+				BEGIN --begin valid user
+					SET @Group_name=(select top 1 group_name from UsersInGroups where User_Id=@User_Id)
+					SET @Next_Recurrence_Date =(select Next_Recurrence_Date from UsersInGroups where User_Id=@User_Id and Group_Name=@Group_Name)
+					DECLARE @Effective_System_date datetime
+					SET @Effective_System_date= (select DATEADD(day,cast((select value from Appconfiguration where [KEY]='DATE_INCREMENT_VALUE')as int),GETDATE()))
+					IF(DATEDIFF(day,@Next_Recurrence_Date,@Effective_System_date)>-1)
+						BEGIN -- start update weekly points
+							declare @Weekly_Points decimal(18,2)
+							set @Weekly_Points=(select Weekly_Points from UsersInGroups where User_Id=@User_Id and Group_Name=@Group_Name)
+							declare @DaysDifference int
+							set @DaysDifference= (select DATEDIFF(day,@Next_Recurrence_Date,@Effective_System_date))
+							declare @Effective_Weeks int
+							SET @Effective_Weeks= (@DaysDifference/7+1)
+							declare @New_Recurrence_Date datetime
+							set @New_Recurrence_Date=(select dateadd(day,(@DaysDifference/7+1)*7,@Next_Recurrence_Date))		
+							
+							set @Points_To_Add=(select (@DaysDifference/7+1)*@Weekly_Points)
+							UPDATE UsersInGroups set Points_Due=Points_Due + @Points_To_Add,
+													Next_Recurrence_Date=@New_Recurrence_Date						
+													where User_Id=@User_Id and Group_Name=@Group_Name				
+						END--end update weekly points	
+					SELECT 'TRUE' as 'RESULT'
+					
+				END--end valid user						 
+		END --@Action = 'VALIDATEUSER'
 END
 GO
 

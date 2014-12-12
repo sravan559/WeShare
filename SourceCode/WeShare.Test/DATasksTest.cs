62a95f55c1f755b88f5e2dccb5b3325aac33bf9a
sandeepgarimella <sandgari@indiana.edu>
2014-12-12 15:43:44 -0500

    Implemented test cases for DATasksTest and DAUserTest

--- SourceCode/WeShare.Test/DATasksTest.cs
+++ SourceCode/WeShare.Test/DATasksTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using WeShare.BusinessModel;
+using System.Collections.Generic;
 
 namespace TestProject1
 {
@@ -88,6 +89,84 @@ namespace TestProject1
             DATasks target = new DATasks(); // TODO: Initialize to an appropriate value
             int TaskId = 3; // TODO: Initialize to an appropriate value
             target.DeleteTask(TaskId);
+        }
+    }
+}
+namespace WeShare.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DATasksTest and is intended
+    ///to contain all DATasksTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DATasksTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GetTasksByGroupName
+        ///</summary>
+        [TestMethod()]
+        public void GetTasksByGroupNameTest()
+        {
+            DATasks target = new DATasks(); // TODO: Initialize to an appropriate value
+            string groupName = string.Empty; // TODO: Initialize to an appropriate value
+            List<TaskInfo> expected = new List<TaskInfo>() {}; // TODO: Initialize to an appropriate value
+            List<TaskInfo> actual;
+            actual = target.GetTasksByGroupName(groupName);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
         }
     }
 }
--- SourceCode/WeShare.Test/DAUserTest.cs
+++ SourceCode/WeShare.Test/DAUserTest.cs
@@ -98,3 +98,78 @@ namespace TestProject1
 
     }
 }
+namespace WeShare.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DAUserTest and is intended
+    ///to contain all DAUserTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DAUserTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for DeleteUser
+        ///</summary>
+        [TestMethod()]
+        public void DeleteUserTest()
+        {
+            DAUser target = new DAUser(); // TODO: Initialize to an appropriate value
+            string userId = "django@gmail.com"; // TODO: Initialize to an appropriate value
+            target.DeleteUser(userId);
+            
+        }
+    }
+}
